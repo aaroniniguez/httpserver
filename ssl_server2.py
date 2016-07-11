b7cb0ead6eb128e92a573c443c8c879d903c1ede
root <root@ip-172-30-0-233.us-west-2.compute.internal>
2016-07-11 22:46:34 +0000

    added ssl

--- ssl_server2.py
+++ ssl_server2.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
-import os, sys
+#import os, sys
 import BaseHTTPServer
 import CGIHTTPServer
 import cgitb; cgitb.enable() ## This line enables CGI error reporting
 import ssl
-from signal import signal, SIGPIPE, SIG_DFL
-from SocketServer import ThreadingMixIn
-import threading
+#from signal import signal, SIGPIPE, SIG_DFL
+#from SocketServer import ThreadingMixIn
+#import threading
 
 server = BaseHTTPServer.HTTPServer
 handler = CGIHTTPServer.CGIHTTPRequestHandler
 class ThreadedHTTPServer(ThreadingMixIn, server):
     """Handle requests in a separate thread."""
 server_address = ("", 8000)
-srvobj = ThreadedHTTPServer(server_address, handler)
-srvobj.socket = ssl.wrap_socket (srvobj.socket, certfile="./localhost.pem", server_side=True)
+handler.cgi_directories = ["/cgi-bin"]
+httpd = server(server_address,handler)
+#srvobj = ThreadedHTTPServer(server_address, handler)
+#srvobj.socket = ssl.wrap_socket (srvobj.socket, certfile="./localhost.pem", server_side=True)
 # Force the use of a subprocess, rather than
 # normal fork behavior since that doesn't work with ssl
-handler.have_fork=False
+#handler.have_fork=False
 srvobj.serve_forever()
