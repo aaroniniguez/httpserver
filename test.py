4bf335c652ecc0fcc021c8b4bdd0d193a066fa5e
root <root@ip-172-30-0-233.us-west-2.compute.internal>
2016-07-12 19:05:38 +0000

    edited test.py to have control headers

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-print "Content-type: text/html"
+print "Access-Control-Allow-Headers: Content-type,Status"
+print "Content-Type: text/html; charset="
+print "Access-Control-Allow-Origin: *"
 print
 print "<title>Test CGI</title>"
 print "<p>Hello k World!</p>"
